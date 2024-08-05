16ad00d9c4fe29bc85e58faec485ecafcedc9741
Ben Dierks <bedi00001@stud.uni-saarland.de>
2024-08-05 18:31:02 +0200

    add: selection of dimension of noise

--- src/main.rs
+++ src/main.rs
@@ -14,32 +14,22 @@ impl NoiseTexture {
         }
     }
 
-    fn scramble(&mut self) {
-        for color in &mut self.data {
-            let rand: u8 = rand::thread_rng().gen::<u8>();
-            *color = rand;
+    fn scramble(&mut self, mode: &ColorMode) {
+        for pixel in &mut self.data.chunks_mut(3) {
+            match mode {
+                ColorMode::Gray => pixel.fill(rand::thread_rng().gen()),
+                ColorMode::RG => {
+                    pixel[0] = rand::thread_rng().gen();
+                    pixel[1] = rand::thread_rng().gen();
+                    pixel[2] = 0;
+                }
+                ColorMode::RGB => pixel.fill_with(|| rand::thread_rng().gen()),
+            }
         }
     }
 
-    fn as_color_image(&self, scale: usize) -> egui::ColorImage {
-        let size = NOISE_SCALE * scale;
-        let mut scaled_image: Vec<u8> = Vec::with_capacity(size * size);
-
-        // upscale the image
-        for y in 0..NOISE_SCALE {
-            let mut row: Vec<u8> = Vec::with_capacity(size);
-            for x in 0..NOISE_SCALE {
-                let index = NOISE_SCALE * y + x;
-                for _ in 0..scale {
-                    let color = &mut self.data[index..index + 3].to_vec();
-                    row.append(color);
-                }
-            }
-            for _ in 0..scale {
-                scaled_image.append(&mut row.clone());
-            }
-        }
-        egui::ColorImage::from_rgb([size, size], &scaled_image)
+    fn as_color_image(&self) -> egui::ColorImage {
+        egui::ColorImage::from_rgb([NOISE_SCALE, NOISE_SCALE], &self.data)
     }
 }
 
@@ -61,7 +51,7 @@ impl MyApp {
         Self {
             texture_handle: None,
             image: NoiseTexture::new(),
-            color_mode : ColorMode::RGB,
+            color_mode: ColorMode::Gray,
         }
     }
 }
@@ -69,27 +59,36 @@ impl MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Load the image into a texture if not already done
-        let space = ctx.available_rect().size();
-        let space = space.x.min(space.y) as usize / 64;
         if self.texture_handle.is_none() {
-            self.image.scramble();
-            self.texture_handle = Some(ctx.load_texture(
-                "color_texture",
-                self.image.as_color_image(space),
-                egui::TextureOptions::default(),
-            ));
+            self.image.scramble(&self.color_mode);
+            self.texture_handle =
+                Some(
+                    ctx.load_texture("color_texture", self.image.as_color_image(), {
+                        egui::TextureOptions {
+                            magnification: egui::TextureFilter::Nearest,
+                            minification: egui::TextureFilter::Nearest,
+                            ..egui::TextureOptions::default()
+                        }
+                    }),
+                );
         } else {
             // Update the existing texture with the new image
-            self.texture_handle.as_mut().unwrap().set(
-                self.image.as_color_image(space),
-                egui::TextureOptions::default(),
-            );
+            self.texture_handle
+                .as_mut()
+                .unwrap()
+                .set(self.image.as_color_image(), {
+                    egui::TextureOptions {
+                        magnification: egui::TextureFilter::Nearest,
+                        minification: egui::TextureFilter::Nearest,
+                        ..egui::TextureOptions::default()
+                    }
+                });
         }
 
         egui::SidePanel::right("Options").show(ctx, |ui| {
             ui.label("Options");
             if ui.button("Scramble").clicked() {
-                self.image.scramble()
+                self.image.scramble(&self.color_mode)
             }
             egui::ComboBox::from_label("")
                 .selected_text(format!("{:?}", self.color_mode))
@@ -104,7 +103,7 @@ impl eframe::App for MyApp {
         });
         egui::CentralPanel::default().show(ctx, |ui| {
             if let Some(texture) = &self.texture_handle {
-                ui.add(egui::Image::new(texture));
+                ui.add(egui::Image::new(texture).fit_to_fraction(egui::vec2(1.0, 1.0)));
             }
         });
 
