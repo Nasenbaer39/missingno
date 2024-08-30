2b7bf35a029bc0afc2cb9e4f8667ee276cd9e626
Ben Dierks <bedi00001@stud.uni-saarland.de>
2024-08-30 17:14:33 +0200

    add: crude blue noise generation

--- src/app/noise.rs
+++ src/app/noise.rs
@@ -1,5 +1,5 @@
-use rand::prelude::*;
 use eframe::egui;
+use rand::prelude::*;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ColorMode {
@@ -8,8 +8,8 @@ pub enum ColorMode {
     Rgb,
 }
 
-// TODO: separate code into modules
-const NOISE_SCALE: usize = 64;
+const NOISE_SCALE: usize = 16;
+const SIGMA: f64 = 2.1_f64;
 
 pub struct NoiseTexture {
     data: [u8; NOISE_SCALE * NOISE_SCALE * 3],
@@ -37,6 +37,77 @@ impl NoiseTexture {
     pub fn as_color_image(&self) -> egui::ColorImage {
         egui::ColorImage::from_rgb([NOISE_SCALE, NOISE_SCALE], &self.data)
     }
+
+    pub fn refine(&mut self) {
+        let first = thread_rng().gen_range(0..NOISE_SCALE * NOISE_SCALE);
+        let mut second = thread_rng().gen_range(0..NOISE_SCALE * NOISE_SCALE - 1);
+
+        if second >= first {
+            second += 1;
+        }
+
+        let initial_energy = self.energy();
+
+        self.swap(first, second);
+
+        let new_energy = self.energy();
+
+        if new_energy >= initial_energy {
+            self.swap(first, second);
+        } else {
+            println!("New energy: {new_energy}");
+        }
+    }
+
+    fn swap(&mut self, first: usize, second: usize) {
+        let color1 = self.data[first * 3];
+        let color2 = self.data[first * 3 + 1];
+        let color3 = self.data[first * 3 + 2];
+
+        self.data[first * 3] = self.data[second * 3];
+        self.data[first * 3 + 1] = self.data[second * 3];
+        self.data[first * 3 + 2] = self.data[second * 3];
+
+        self.data[second * 3] = color1;
+        self.data[second * 3 + 1] = color2;
+        self.data[second * 3 + 2] = color3;
+    }
+
+    fn energy(&self) -> f64 {
+        let mut overall_energy = 0.0;
+        for i in 0..NOISE_SCALE * NOISE_SCALE {
+            for j in i..NOISE_SCALE * NOISE_SCALE {
+                if i == j {
+                    continue;
+                }
+
+                let mut energy = (-self.distance_sqr(i, j) / SIGMA.powi(2)).exp();
+                energy /= (1.0 + f64::from(self.data[i * 3].abs_diff(self.data[j * 3]))).sqrt();
+                overall_energy += energy;
+            }
+        }
+
+        overall_energy
+    }
+
+    fn distance_sqr(&self, first: usize, second: usize) -> f64 {
+        let x1 = first % NOISE_SCALE;
+        let x2 = second % NOISE_SCALE;
+
+        let y1 = first / NOISE_SCALE;
+        let y2 = second / NOISE_SCALE;
+
+        let mut x = x1.abs_diff(x2);
+        let mut y = y1.abs_diff(y2);
+
+        if x > NOISE_SCALE / 2 {
+            x = NOISE_SCALE - x;
+        }
+
+        if y > NOISE_SCALE / 2 {
+            y = NOISE_SCALE - y;
+        }
+
+        return (x * x + y * y) as f64;
+    }
 }
-
-
